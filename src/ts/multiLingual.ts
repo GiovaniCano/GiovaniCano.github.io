2c71d293231a5bfe7b4a0b86519ca54f9330b442
Giovani <giovanicnvg@gmail.com>
2022-05-10 11:36:24 -0500

    Added 3 language buttons and the main multiLingual function

--- src/ts/multiLingual.ts
+++ src/ts/multiLingual.ts
@@ -1,9 +1,32 @@
 function multiLingual() {
-    
+    const langIcons = document.querySelectorAll(".js-lang-icon")! as NodeListOf<HTMLElement>
+    langIcons.forEach(icon=>{
+        icon.onclick = e => {
+            e.preventDefault()
+            langIcons.forEach(icon=>{
+                const en = icon.querySelector('[data-langIcon="en"]')
+                const es = icon.querySelector('[data-langIcon="es"]')
+
+                if(!en || !es) return
+
+                if(en.classList.contains("active-lang")) { // en to es
+                    en.classList.remove("active-lang")
+                    es.classList.add("active-lang")
+                    translate("es")
+                } else { // es to en
+                    en.classList.add("active-lang")
+                    es.classList.remove("active-lang")
+                    translate("en")
+                }
+            })
+        }
+    })
 }
 
 async function translate(language: string) {
-    const response = await fetch(`/languages/{language}.json`)
+    if(!["en", "es"].includes(language)) return
+    
+    const response = await fetch(`/languages/${language}.json`)
     const texts = await response.json()
 
     const elementsToTranslate = document.querySelectorAll("[data-lang]") as NodeListOf<HTMLElement>
